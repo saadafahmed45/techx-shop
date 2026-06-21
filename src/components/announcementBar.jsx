44a530e8e8cc92832d2da21320d0d54ba7e57f54
saadafahmed45 <hafijnahid45@gmail.com>
2026-06-22 01:48:45 +0600

    added

--- src/components/announcementBar.jsx
+++ src/components/announcementBar.jsx
@@ -1,7 +1,7 @@
 const AnnouncementBar = () => {
     return (
         <div>
-            <div className="bg-white text-black py-4 px-4 text-center text-sm md:text-md font-medium border-b border-slate-800">
+            <div className="bg-white text-black py-4 px-4 text-center text-sm md:text-md     font-medium border-b border-slate-800">
       <div className="max-w-7xl mx-auto animate-[marquee_10s_linear_infinite]">
         Shop smarter with FREE next-day delivery on orders over $500. ✨
       </div>
